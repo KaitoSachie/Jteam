06e83847502360facd0aa080deeef9884fe8e4f6
kaito <roorinngurooru@gmail.com>
2016-10-13 11:49:39 +0900

    除外するフォルダの追加

--- KokiSeisaku/BaseCrossDx11/Character.cpp
+++ KokiSeisaku/BaseCrossDx11/Character.cpp
@@ -30,7 +30,7 @@ namespace basecross{
 		ParticlePtr->SetMaxTime(0.5f);
 		vector<ParticleSprite>& pSpriteVec = ParticlePtr->GetParticleSpriteVec();
 		for (auto& rParticleSprite : ParticlePtr->GetParticleSpriteVec()) {
-			rParticleSprite.m_LocalPos.x = Util::RandZeroToOne() * 0.1f - 0.05f;
+			//rParticleSprite.m_LocalPos.x = Util::RandZeroToOne() * 0.1f - 0.05f;
 			rParticleSprite.m_LocalPos.y = Util::RandZeroToOne() * 0.1f;
 			rParticleSprite.m_LocalPos.z = Util::RandZeroToOne() * 0.1f - 0.05f;
 			//各パーティクルの移動速度を指定
